9da6e6b09fabf912c6406f208edc8928c6a777ea
Astro <118923529+knucksbendy@users.noreply.github.com>
2025-05-12 21:22:12 +0300

    spele
    12.05

---
+++ HH/guessthenumber.js
@@ -0,0 +1,31 @@
+let targetNumber=
+Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+function checkGuess() {
+    const guess =
+    parseInt(document.getElementById("guessInput").value);
+    const message =
+    document.getElementById("message");
+    attempts++;
+console.log('Mēģinājums');
+    if (isNaN(guess) || guess < 1 || guess > 100 ) {
+        alert(
+        "Lūdzu ievadi skaitli no 1 līdz 100.");
+        message.textContent =
+        "Lūdzu ievadi skaitli no 1 līdz 100."
+        return;
+    }
+if (guess === targetNumber) {
+    message.textContent =
+    'Apsveicu! Tu uzminēji skaitli!';
+}
+else if (guess < targetNumber) {
+    message.textContent =
+    "Skaitlis ir lielāks!";
+}
+else {
+    message.textContent =
+    "Skaitlis ir mazāks!";
+} }
+
